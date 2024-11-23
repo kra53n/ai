f002afd4b3aea32c35a04cbd7bb25e350ed7347e
kra53n <greasha46@gmail.com>
2024-11-24 01:16:52 +0300

    -

--- 1/Measures.cs
+++ 1/Measures.cs
@@ -11,13 +11,13 @@ class Measures
     public static void Do()
     {
         (string name, Func<ISearcher<List<State>>> factory)[] searches = {
-            ("breadth", () => new Searcher(Searcher.Type.Breadth)),
+            //("breadth", () => new Searcher(Searcher.Type.Breadth)),
             ("depth", () => new Searcher(Searcher.Type.Depth)),
-            ("depth_iterative", () => new DepthFirstSearch()),
-            ("bidirectional", () => new BidirectionalSearch()),
-            ("heuristic1", () => new InformedSearch(InformedSearch.MidHeuristic, "mid")),
-            ("heuristic2", () => new InformedSearch(InformedSearch.BetterHeuristic, "получше")),
-            ("heuristic3", () => new InformedSearch(InformedSearch.HorribleHeuristic, "худшего")),
+            //("depth_iterative", () => new DepthFirstSearch()),
+            //("bidirectional", () => new BidirectionalSearch()),
+            //("heuristic1", () => new InformedSearch(InformedSearch.MidHeuristic, "mid")),
+            //("heuristic2", () => new InformedSearch(InformedSearch.BetterHeuristic, "получше")),
+            //("heuristic3", () => new InformedSearch(InformedSearch.HorribleHeuristic, "худшего")),
         };
 
         Directory.CreateDirectory("../../../measures");
