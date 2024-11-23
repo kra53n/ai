e9afde827eac434f2903c91168bd08409aa40b67
kra53n <greasha46@gmail.com>
2024-11-23 19:57:39 +0300

    add script to process measurements

---
+++ 1/process_measures.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+
+
+p = Path("measures")
+searches = ("breadth", "depth", "depth_iterative", "bidirectional", "heuristic1", "heuristic2", "heuristic3")
+ds = (2, 7, 12, 22)
+maps = tuple(range(1, 11))
+
+
+for search in searches:
+    iters = [0]*len(ds)
+    Ns = [0]*len(ds)
+    for d in ds:
+        for m in maps:
+            try:
+                with open(p / f'{search}_level{d}-{m}.txt', 'r') as f:
+                    iterr, N = map(int, (i.strip().split()[1] for i in f.readlines()))
+                    iters[ds.index(d)] += iterr
+                    Ns[ds.index(d)] += N
+            except FileNotFoundError:
+                continue
+    iters = [float(i) / len(maps) for i in iters]
+    Ns = [float(i) / len(maps) for i in Ns]
+    print(search.capitalize())
+    print(*iters)
+    print(*Ns)
+    print()
