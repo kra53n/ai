b311c131f462d8fabce0cc49cd339beb5f5ca0d0
kra53n <greasha46@gmail.com>
2025-07-21 09:39:00 +0300

    print missing files

--- 1/process_measures.py
+++ 1/process_measures.py
@@ -5,6 +5,7 @@ p = Path("measures")
 searches = ("breadth", "depth", "depth_iterative", "bidirectional", "heuristic1", "heuristic2", "heuristic3")
 ds = (2, 7, 12, 17, 22)
 maps = tuple(range(1, 11))
+files_not_found = []
 
 
 for search in searches:
@@ -12,13 +13,14 @@ for search in searches:
     Ns = [0]*len(ds)
     for d_idx, d in enumerate(ds):
         for m in maps:
+            filename = f'{search}_level{d}-{m}.txt'
             try:
-                with open(p / f'{search}_level{d}-{m}.txt', 'r') as f:
+                with open(p / filename, 'r') as f:
                     iterr, N = map(int, (i.split()[1] for i in f.readlines()))
                     iters[d_idx] += iterr
                     Ns[d_idx] += N
             except FileNotFoundError:
-                # print(f'{search}_level{d}-{m}.txt')
+                files_not_found.append(filename)
                 continue
         iters[d_idx] /= len(maps)
         Ns[d_idx] /= len(maps)
@@ -26,3 +28,6 @@ for search in searches:
     print(*iters)
     print(*Ns)
     print()
+
+print('Files were not found')
+print('\n'.join(files_not_found))
